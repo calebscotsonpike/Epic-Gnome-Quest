eb6b9423ad007102ea06270eff1dd1790da09716
Josh <joshua.pike25@gmail.com>
2020-10-04 10:12:55 +0100

    Imported dialogue tree functionality.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+
 tree = ET.parse('Text.xml')
 root = tree.getroot()
 
@@ -14,7 +15,6 @@ class NPC:
     name = "John"
     num = "001"
 
-
 def game():
     """Primary game while loop"""
     npc = NPC
@@ -22,47 +22,57 @@ def game():
         talk(npc)
         break
 
+
 def talk(npc):
     """Conversation while loop (will be)"""
-    getDialogue(npc, None)
+    name = npc.name
+    num = npc.num
     while True:
+        conversation(name, num, None)
         response = input()
-        finished = getDialogue(npc, response)
-        if finished == 0:
-            break;
+        if response in ['0','1']:
+            num = conversation(name, num, response)
+        else:
+            print('INVALID')
+        if num == '0':
+            break
 
-def getDialogue(npc, response):
+
+def conversation(name, num, response):
     """XML parser.
 
-            iterates through XML comversation tree to extract the correct dialogue. 'line' is the desired xml tag,
+            iterates through XML conversation tree to extract the correct dialogue. 'dialogue' is the desired xml tag,
             this nested structure can be searched through for the correct responses.
 
             Parameters
             ----------
-            npc : object, neccessary
+            name : NPC name
+            num : object, necessary
                 the person the hero is speaking to
             response : str, optional
                 the user input (default is None)
     """
     for branch in root.iter('data'):
-        for line in branch:
-            if line.get("id") == npc.num:
+        for dialogue in branch:
+            if dialogue.get("id") == num:
                 if None == response:
                     # get initial speech
-                    for message in line.iter('message'):
-                        print(npc.name + ": " + message.text)
+                    for message in dialogue.iter('message'):
+                        print(name + ": " + message.text)
                     i = 0
-                    for choice in line.iter('command'):
+                    for choice in dialogue.iter('command'):
                         print(str(i) + ": " + choice.text)
                         i += 1
                 else:
                     # options after user input
-                    for result in line.iter('choice'):
-                        if result.get("id") == response:
-                            for output in result.iter('result'):
-                                print(output.text)
-                                return 0
+                    for choice in dialogue.iter('choice'):
+                        if choice.get("id") == response:
+                            for result in choice.iter('result'):
+                                if result.text != None:
+                                    print(result.text)
+                                num = result.get("id")
+                                return num
+
 
 if __name__ == '__main__':
     game()
-
